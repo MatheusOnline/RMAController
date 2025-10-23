48d075cb893f446381842038bee1e4353c0b2491
Matheus_Fsilva <matheus.francisco20021@gmail.com>
2025-10-23 17:46:47 -0300

    Resolvendo 0.1.5

---
+++ src/Teste.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./container/Home";
+import RegistrationLabel from "./container/registrationLabel"
+import ShopeeAuth from "./container/Auth";
+function Teste() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        
+        <Route path="/leitor" element={<RegistrationLabel />} />
+
+        <Route path="/auth" element={<ShopeeAuth/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default Teste;
