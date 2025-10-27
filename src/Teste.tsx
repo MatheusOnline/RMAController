45c537f324d2f5e4dcf9c7ca554c8772e6a256f4
Matheus_Fsilva <matheus.francisco20021@gmail.com>
2025-10-27 19:14:22 -0300

    pegando as devolucoes 0.1

--- src/Teste.tsx
+++ src/Teste.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./container/Home";
 import RegistrationLabel from "./container/registrationLabel"
 import ShopeeAuth from "./container/Auth";
+import Return from "./container/Return";
+
 function Teste() {
   return (
     <Router>
@@ -11,6 +13,7 @@ function Teste() {
         <Route path="/leitor" element={<RegistrationLabel />} />
 
         <Route path="/auth" element={<ShopeeAuth/>}/>
+        <Route path="/return" element={<Return/>}/>
       </Routes>
     </Router>
   );
