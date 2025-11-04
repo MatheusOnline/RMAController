570f8ff669c03626f1661d50f4a4b014e901566d
Matheus_Fsilva <matheus.francisco20021@gmail.com>
2025-11-04 19:05:25 -0300

    Refatorando o codigo

--- src/container/Home.tsx
+++ src/container/Home.tsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 import { GlobalStyle } from '../styles/GlobalStyle.tsx';
-import { useEffect, useState } from 'react';
+
 //components
 import CardIndex from '../components/cardIndex.tsx'
 import ReturnTable from '../components/returnTable.tsx'
 import Header from '../components/header/header.tsx';
 //Services
-import Apiget from "../services/Apiget.tsx"
+
 
 
 
@@ -20,32 +20,20 @@ const ListCard = styled.div`
 
 
 function Home (){
-     const [data, setData] = useState<any[]>([]);
-     useEffect(() => {
-      async function fetchData() {
-        try {
-          const res = await Apiget();
-          setData(res)
-        } catch (error) {
-          console.error("Erro ao buscar dados:", error);
-        }
-      }
-    
-      fetchData();
-    }, []);
+     
 
     return(
         <>
             <GlobalStyle />
             <Header/>
             <ListCard>
-                <CardIndex descript="Total de Casos" count={data.length} color="#125f8b" />
+                
                 <CardIndex descript="Total de Casos Ativos" count={12} color="#125f8b" />
                 <CardIndex descript="Aguardando Recebimento/Análise" count={32} color="#125f8b" />
                 <CardIndex descript="Casos resolvidos" count={32} color="#125f8b" />
             </ListCard>
             
-            <ReturnTable data={data}/>
+            
         
         </>
 
