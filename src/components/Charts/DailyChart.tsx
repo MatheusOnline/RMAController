8f56f587950bf3ea8589b235ebc479e08bf100c0
Matheus_Fsilva <matheus.francisco20021@gmail.com>
2025-11-21 16:31:04 -0300

    dados da dashboard

--- src/components/Charts/DailyChart.tsx
+++ src/components/Charts/DailyChart.tsx
@@ -3,7 +3,7 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts"
 
 function DailyChart({data}:any) {
   return (
-    <LineChart width={500} height={300} data={data.map((i:any) => ({ date: i._id, total: i.total }))}>
+    <LineChart width={500} height={300} data={data}>
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="dia" />
       <YAxis />
